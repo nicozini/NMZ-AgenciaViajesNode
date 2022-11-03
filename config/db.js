979ae4cb68efb1b87d819767fe152b2dbd079edc
nicozini <nicolasmartinzini@outlook.com>
2022-11-03 18:59:44 -0300

    Update

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv/config';
 // Sequelize lo llama sequelize pero le puedo poner db y luego exportar db
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST, // 'localhost'  o  '127.0.0.1'
-    dialect: process.env.DB_DIALECT,
+    dialect: 'mysql',
     port: process.env.DB_PORT, // 3306  o  '3306' Lo pone como string
     define: {
         timestamps: false
